2e8ef951787492bf461186559870e4ed86c7d484
Dave Durbin <scoobadood@me.com>
2016-11-13 14:22:13 +1100

    feature: Added PDSF file reading functionality
    Code now parses these files

--- src/SceneFlowAlgorithm/PDSFMockSceneFlowAlgorithm.cpp
+++ src/SceneFlowAlgorithm/PDSFMockSceneFlowAlgorithm.cpp
@@ -19,24 +19,48 @@ bool PDSFMockSceneFlowAlgorithm::read_scene_flow( const std::string & file_name,
 		};
 
 
-	struct ss {
-
+	// Structure to handle processing line
+	struct SceneFlowData {
+		// File name to read
 		std::string file_name;
+
+		// Pointer to the pareent lagorithm
 		PDSFMockSceneFlowAlgorithm * parent;
 
-		ss( PDSFMockSceneFlowAlgorithm* container, const std::string& file_name ) {
+		// Vector of displacements
+		std::vector<float3> v;
+
+		// Image dimensions
+		int image_width = 0;
+		int image_height = 0;
+
+		// Construct one, stash the parent 
+		// Also read the last line fo the file to obtain the X,Y pixel dimensions
+		SceneFlowData( PDSFMockSceneFlowAlgorithm* container, const std::string& file_name ) {
 			this->file_name = file_name;
 			parent = container;
+
+			// Read last line of file
+			std::string last_line;
+			if( read_last_line( file_name, last_line )) {
+				// Parse dimensions from first two elements
+				float dims[2];
+				if ( ! parent->read_floats_from_string( last_line.c_str(), 2, dims ) ) {
+					std::cerr << "Error: Problem reading dimensions from PD scene flow file " << std::endl;
+				} else {
+					image_width = (int)dims[0];
+					image_height = (int)dims[1];
+				}
+			}
 		}
 
-		std::vector<float3> v;
 
 
 		void process_line( const std::string& line ) {
 			// Line is int x, int y, float sfx, sfy, sfz
 			float values[5];
 
-			if ( ! parent->read_floats_from_string( file_name.c_str(), 5, values ) ) {
+			if ( ! parent->read_floats_from_string( line.c_str(), 5, values ) ) {
 				std::cerr << "Error: Problem entries from PD scene flow file " << std::endl;
 			} else {
 				int px = (int)values[0];
@@ -46,11 +70,29 @@ bool PDSFMockSceneFlowAlgorithm::read_scene_flow( const std::string & file_name,
 		}
 	};
 
-	ss s{ this, file_name };
+	SceneFlowData sfd{ this, file_name };
 	
-	std::function<void( const std::string & )> f = std::bind(&ss::process_line, s, std::placeholders::_1 );
+	std::function<void( const std::string & )> f = std::bind(&SceneFlowData::process_line, sfd, std::placeholders::_1 );
 
 	bool read_ok = process_file_by_lines( file_name, f );
+
+	// Work out how many elements we got and transfer them to the Eigen shape we got passed in residuals
+	int num_values = sfd.image_width * sfd.image_height;
+
+	residuals.resize( 3, num_values);
+	for ( size_t i = 0; i < num_values; i++ ) {
+		residuals(0, i) = sfd.v[i].x;
+		residuals(1, i) = sfd.v[i].y;
+		residuals(2, i) = sfd.v[i].z;
+	}
+
+	// Set default global rotation and translation
+	translation[0] = 0.0f;
+	translation[1] = 0.0;
+	translation[2] = 0.0;
+	rotation[0] = 0.0;
+	rotation[1] = 0.0;
+	rotation[2] = 0.0;
 
 	return read_ok;
 }
