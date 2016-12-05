e3ddb6d745cd6e856107765846dd99fa71ec297a
Dave Durbin <scoobadood@me.com>
2016-12-05 15:53:32 +1100

    test: Updated test resolution

--- src/SceneFusion/SceneFusion.cpp
+++ src/SceneFusion/SceneFusion.cpp
@@ -34,11 +34,11 @@
 SceneFusion::SceneFusion( SceneFlowAlgorithm * sfa, RGBDDevice * rgbd_device ) {
 
 	// Construct the TSDFVolume
-	m_volume = new TSDFVolume(400, 400, 400, 2000, 2000, 2000);
+	m_volume = new TSDFVolume(450, 450, 450, 2000, 2000, 2000);
 //	m_volume->offset( -1500, -1500, -1500);
 
 	// And camera (from FREI 1 IR calibration data at TUM)
-	m_camera = new Camera{ 591.1f, 590.1f, 331.0f, 234.6f };
+	m_camera = Camera::default_depth_camera();
 
 	// Pose the camera
 	m_camera->move_to( 1000, 1000, 0 );
